4d4fec30e2a55383bdcd67260cb369e80f37d497
leonardo <leonardo.brasil59@outlook.com>
2020-02-20 15:52:21 -0300

    :rocket: first test routes

--- src/server.js
+++ src/server.js
@@ -1,21 +1,3 @@
-import express from 'express';
-import routes from './routes';
+import app from './app';
 
-class App {
-  constructor(){
-    this.server = express();
-
-    this.middlewares();
-    this.routes();
-  }
-
-  middlewares(){
-    this.server.use(express.json());
-  }
-  
-  routes(){
-    this.server.use(routes);
-  }
-}
-
-export default new App().server;
+app.listen(3333);
