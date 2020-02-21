b17a0062d26605e72b1f01f0eda5c4598484b1b6
leonardo <leonardo.brasil59@outlook.com>
2020-02-21 14:40:50 -0300

    :rocket: creating user registration

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/users', async (req, res) => {
-  const user = await User.create({
-    name: 'Leonardo Brasil',
-    email: 'leonardo.brasil59@outlook.com',
-    password_hash: '123abc',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
