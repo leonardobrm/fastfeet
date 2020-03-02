4e3d2fb814e7367dbbbdb4a2b70e48ae277566dc
leonardo <leonardo.brasil59@outlook.com>
2020-03-02 16:06:59 -0300

    :rocket: recipient registration

--- src/app/controllers/RecipientsController.js
+++ src/app/controllers/RecipientsController.js
@@ -1,7 +1,37 @@
+import * as Yup from 'yup';
 import Recipients from '../models/Recipients';
 
 class RecipientsController {
   async store(req, res) {
+    const schema = await Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number()
+        .required()
+        .min(1),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.string()
+        .required()
+        .min(9),
+      ssn: Yup.number()
+        .required()
+        .min(9),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const RecipientsExists = await Recipients.findOne({
+      where: { ssn: req.body.ssn },
+    });
+
+    if (RecipientsExists) {
+      return res.status(400).json({ error: 'Recipients already exists' });
+    }
+
     const {
       name,
       street,
